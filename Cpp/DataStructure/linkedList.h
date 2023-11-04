c4ca875d8f89c99d4a2da1e0ef7cba6a88d2f282
Nodelank <carbonium3@gmail.com>
2023-11-05 03:27:47 +0900

    Cpp linked list (#20)
    * Example, LinkedList
    
    * DoublyLinkedList & fix LinkedList

--- Cpp/DataStructure/linkedList.h
+++ Cpp/DataStructure/linkedList.h
@@ -1,21 +1,58 @@
 #ifndef CARBON_LINKED_LIST_H
 #define CARBON_LINKED_LIST_H
-#include "node.h"
+
 class LinkedList
 {   
 private:
-
-public:
-    int data;
+    struct Node
+    {
+       int data;
+       Node* next;
+    };
     Node* head;
-
+    Node* tail;
+    
+  
+public:
     LinkedList() {
-       this->head = nullptr;
+        head = nullptr;
+        tail = nullptr;
     }
+    ~LinkedList();
 
-    void appendNode(int data);
+    void insertNode(int data)
     void deleteNode(int data);
     void printList();
 }; 
+
+
+/**
+ * 이중 연결리스트
+*/
+class DoublyLinkedList
+{
+private:
+    struct Node
+    {
+       int data;    //노드에 저장할 데이터
+       Node* prev;  //앞 노드를 가르키는 포인터
+       Node* next;  //뒷 노드를 가르키는 포인터
+    };
+    Node* head;
+    Node* tail;
+    int size;
+
+public:
+    DoublyLinkedList();
+    ~DoublyLinkedList();
+
+   void insertHead(int data); //연결리스트의 제일 앞부분추가
+   void insertTail(int data); //연결리스트의 제일 뒷부분추가
+   void deleteHead(int data);
+   void deleteTail(int data);
+   Node* serarchNode(Node* head, int data);
+   void printList();
+   bool is_empty();
+};
 
 #endif
